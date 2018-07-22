b3832f1d6cc4e250e6c289080bdc274a3126142a
AntonNiko <ant.nikitenko@gmail.com>
2018-07-22 09:58:26 -0700

    Reformatted buses.csv, begun Java GUI application, added BC Transit open data as reference for information

---
+++ TransitGUI/src/MainGUI.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class MainGUI extends Frame implements ActionListener {
+
+	public static void main(String[] args)
+	{
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}

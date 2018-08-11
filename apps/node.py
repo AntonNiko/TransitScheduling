e48c11d3e3698782a54a795e3a7e5d7138e0e923
AntonNiko <ant.nikitenko@gmail.com>
2018-08-10 20:36:51 -0700

    Added function evaluateConnectionTime to node.py to evluate connection quality

--- apps/node.py
+++ apps/node.py
@@ -1,4 +1,5 @@
 from itertools import permutations
+from datetime import datetime, time, timedelta
 import csv
 
 PROJECT_ROOT_DATA_DIR = "data/"
@@ -9,6 +10,9 @@ class Node():
     node_name = None
     node_stops = []
     connections = []
+
+    ## Performance variables
+    total_wait_time = 0
     
     def __init__(self, node_id, node_name, node_stops):
         self.node_id = node_id
@@ -22,9 +26,28 @@ class Node():
         self.connections = list(permutations(self.node_stops, 2))
 
     def evaluateConnectionTime(self, trips):
+        ## Trip JSON structure: trips[route][trip_id][stop] : time
         print("Evaluating Node: {} --- {}".format(self.node_id, self.node_name))
         for connection in self.connections:
+            start_stop = connection[0]
+            finish_stop = connection[1]
             print(connection)
+            for start_trip_id in trips[start_stop[0]]:
+                start_time = trips[start_stop[0]][start_trip_id][start_stop[1]]
+                start_time_formatted = datetime.strptime(start_time, "%H:%M:%S").time()
+                ## Cycle through each departure time of end connection until the next connection
+                ## is found
+                for stop_trip_id in trips[finish_stop[0]]:
+                    stop_time = trips[finish_stop[0]][stop_trip_id][finish_stop[1]]
+                    stop_time_formatted = datetime.strptime(stop_time, "%H:%M:%S").time()
+                    if stop_time_formatted > start_time_formatted:
+                        ## TODO: Catch situations when wait time calculations result in 0
+                        self.total_wait_time+=(stop_time_formatted.minute - start_time_formatted.minute)
+
+                    
+                    
+                
+            
         
             
           
